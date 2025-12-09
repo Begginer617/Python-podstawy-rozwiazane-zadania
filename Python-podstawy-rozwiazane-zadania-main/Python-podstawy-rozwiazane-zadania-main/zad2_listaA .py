efc824b36df5f262717cecfb4f1f9ae97de75ccd
milosz <skorupskimilosz@gmail.com>
2025-12-10 00:14:48 +0100

    zad2_in_progress

--- Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad2_listaA .py
+++ Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad2_listaA .py
@@ -1,16 +1,14 @@
 # Napisz program zawierający funkcję zwracającą silnię liczby podanej jako argument. Wskazówka: Policz silnię iteracyjnie
 
-# nie chciałem kombinoac i chciałem wykorzystac juz funkcje ktora istnieje w pythonie
-
-
 import math
 
 
-def silnia_liczby_podanej_jako_argument():
+def silnia_iteracyjna_liczby_podanej_jako_argument():
+
     x = int(input("Podaj liczbę: "))
+    wynik = 1
+    for i in range(1, x + 1):
+    wynik = wynik * i
+    print(wynik)
 
-    math.factorial(x)
-    print(math.factorial(x))
-
-
-silnia_liczby_podanej_jako_argument()
+silnia_iteracyjna_liczby_podanej_jako_argument()
