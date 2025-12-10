4a9fa080f28e94087ffa252dcc6ba2ad799a035a
milosz <skorupskimilosz@gmail.com>
2025-12-10 23:59:12 +0100

    zad6 rozwiazane

--- Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad4_listaA .py
+++ Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad4_listaA .py
@@ -19,11 +19,24 @@ def policz():
 
 def choinka():
     max_gwiazdek = policz()            # liczba gwiazdek w największym wierszu przypisana do zmiennej max_gwiazdek
-    h = (max_gwiazdek + 1) // 2        # obliczamy wysokość choinki, Klasyczna choinka w konsoli ma nieparzystą liczbę gwiazdek w każdym wierszu: 1, 3, 5, …, największa liczba = 2*h - 1.
-    for i in range(1, h + 1):           # zapetlenie + atrybuty petli
-        spacje = " " * (h - i)          # liczba spacji z lewej strony
+    h = (max_gwiazdek + 1) // 2        # obliczamy wysokość choinki, Klasyczna choinka w konsoli ma nieparzystą liczbę gwiazdek w każdym wierszu: 1, 3, 5, …, największa liczba = 2*h - 1 = czyli 15
+    for i in range(1, h + 1):           # zapętlenie + atrybuty pętli
+        spacje = " " * (h - i)          # liczba spacji z lewej strony 15-i=7 daje 8
         gwiazdki = "*" * (2 * i - 1)    # liczba gwiazdek w tym wierszu
-        print(spacje + gwiazdki)        # print zmiennych zeby powstała choinka
+        print(spacje + gwiazdki)        # print zmiennych, żeby powstała choinka
 
 choinka()
+
+
+#
+# | i (wiersz) | spacje (`h - i`) | gwiazdki (`2*i - 1`) | wizualizacja      |
+# | ---------- | ---------------- | -------------------- | ----------------- |
+# | 1          | 7                | 1                    | `       *`        |
+# | 2          | 6                | 3                    | `      ***`       |
+# | 3          | 5                | 5                    | `     *****`      |
+# | 4          | 4                | 7                    | `    *******`     |
+# | 5          | 3                | 9                    | `   *********`    |
+# | 6          | 2                | 11                   | `  ***********`   |
+# | 7          | 1                | 13                   | ` *************`  |
+# | 8          | 0                | 15                   | `***************` |
 
