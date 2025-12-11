fc96447d25e2f1aef99a6d9c0d9185d86ef13aaf
milosz <skorupskimilosz@gmail.com>
2025-12-11 20:42:54 +0100

    dodanie zadanka

---
+++ Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/dodatkowe_zadanko.py
@@ -0,0 +1,39 @@
+def cwiczenie():  # TEGO NIE ZMIENIAJ, TO MUSI BYĆ NA SAMEJ GÓRZE
+
+    # TU ROZPOCZNIJ KODOWAĆ
+    # Samochód na 100 km spala 8 l paliwa.
+    # Ile spali paliwa po przejechaniu 488 km ?
+
+    dystans_do_zrobienia = 488
+    zuzycie_paliwa_na_100 = 8
+    dystans_100km = 100
+
+    wynik = (dystans_do_zrobienia * zuzycie_paliwa_na_100) / dystans_100km
+
+    return round(wynik, 2)  # TEGO NIE ZMIENIAJ, TO MUSI BYĆ NA SAMYM DOLE
+print(cwiczenie())
+
+
+def cwiczenie():  # TEGO NIE ZMIENIAJ, TO MUSI BYĆ NA SAMEJ GÓRZE
+
+    # TU ROZPOCZNIJ KODOWAĆ
+
+    bok_figury_geom = int(input("Podaj bok: "))
+    print(bok_kwadratu)
+    a = int(input("Podaj bok a: "))
+    b = int(input("Podaj bok b: "))
+    c = int(input("Podaj bok c: "))
+    print("Długości trójkąta:", a, b, c)
+
+    poleKwadratu = bok_kwadratu ** 2
+    print(poleKwadratu)
+
+    obwodTrojkata = (a + b + c)
+    print(obwodTrojkata)
+    poleKola =
+    poleTrojkata =
+
+    return (round(poleKwadratu, 2), round(obwodTrojkata, 2), round(poleKola, 2),
+            round(poleTrojkata, 2))  # TEGO NIE ZMIENIAJ, TO MUSI BYĆ NA SAMYM DOLE
+    print
+    cwiczenie()
