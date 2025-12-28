c586d4d6ebb8702102d91c490dc79c7decee825a
milosz <skorupskimilosz@gmail.com>
2025-12-28 01:47:57 +0100

    zadania_cwiczenia

--- Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad5_listaB .py
+++ Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad5_listaB .py
@@ -1,3 +1,11 @@
 # Zadanie 5
 # Napisz program wyświetlający pierwszy i ostatni kolor z podanej listy:
 # Kolory = [‘czerwony’, ‘zielony’, ‘niebieski’, ‘czarny’]
+
+def zadanie5b():
+    kolory = ["czerwony", "zielony", "niebieski", "czarny"]
+
+    print(kolory[0], kolory[-1]) #-1 oznacza zawsze ostatni element listy
+    print(kolory[0], kolory[3])
+
+zadanie5b()
--- Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad6_listaB .py
+++ Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad6_listaB .py
@@ -1,2 +1,22 @@
 # Zadanie 6
 # Napisz program zliczający liczbę 4 w podanej liście
+
+def zadanie6b():
+    lista_czworek = [
+        "4456.44", "234564", "224562", "124563", "4.42344",
+        "234", "222", "123", "4.42344", "879789",
+        "567", "457", "789", "90", "12", "25", "68", "90"
+    ]
+
+    lista_czworek.sort()
+    print(lista_czworek.sort())
+
+    licznik = 0
+
+    for i in lista_czworek:
+        print(i)
+        licznik += i.count("4")
+
+    print("Cyfra '4' występuje:", licznik, "razy")
+
+zadanie6b()
