d34d901e98f084d028db6ca7cd3a8421511e01f1
Milosz Skorupski <skorupskimilosz@gmail.com>
2025-12-28 19:02:02 +0100

    zadanko

--- Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad12_listaB .py
+++ Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/Python-podstawy-rozwiazane-zadania-main/zad12_listaB .py
@@ -6,3 +6,19 @@
 # Wyjście :
 # LITERY 10
 # CYFRY 3
+
+def zadanie12b():
+    tekst = "hello world! 123"
+    litery = 0
+    cyfry = 0
+
+    for znak in tekst:
+        if znak.isalpha():
+            litery += 1
+        elif znak.isdigit():
+            cyfry += 1
+
+    print("LITERY", litery)
+    print("CYFRY", cyfry)
+
+zadanie12b()
